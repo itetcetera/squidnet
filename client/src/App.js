6fd1e9eaa7326463537943e958f89e4b5ed346fd
themac1010 <connorjmacpherson@gmail.com>
2021-01-09 20:17:25 -0500

    Foundation for HomePage

--- client/src/App.js
+++ client/src/App.js
@@ -6,28 +6,14 @@ import {
   Route
 } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
+import HomePage from "./core/homepage"
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to Hello Mark.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
-          </div>
+          <HomePage />
         </Route>
       </Switch>
     </Router>
---
+++ client/src/core/homepage/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function HomePage(){
+
+    return (
+        <div>
+            POG
+        </div>
+    )
+}
+
+export default HomePage
