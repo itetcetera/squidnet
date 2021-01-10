0352150978ab657965539113ed87801a1fc3da95
Udit Kapoor <ukapoor.29@gmail.com>
2021-01-10 08:48:54 +0530

    tried sockets

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_socketio import SocketIO, send
 
-app = Flask(__name__, static_folder='./client/build', static_url_path='/',)
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'poggers'
 
-socketIo = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app)
 
 app.debug = True
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
-@socketIo.on("message")
-def handleMessage(msg):
-    print(msg)
-    send(msg, broadcast=True)
-    return None
+@app.route('/chat')
+def chat():
+    username = request.args.get('username')
+    room = request.args.get('room')
+
+    if username and room:
+        return render_template('chat.html')
+    else:
+        return redirect('/')
+
+@socketio.on('join_room')
+def handle_join_room_event(data):
+    for d in data:
+        print(d)
+    app.logger.info("{} has joined the room {}".format(data.get('username'), data['room']))
 
 if __name__ == '__main__':
-    socketIo.run(app)
+    socketio.run(app)
+    
